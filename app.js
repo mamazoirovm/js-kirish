cb56b9a5e74c896822b941ca4808aea8da603fbc
Mamazoirov Muhammadqodir <121937015+mamazoirovm@users.noreply.github.com>
2023-10-07 07:51:13 +0500

    masalalar qo'shildi
    masala qoshildi 2ta

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-let son1 = prompt("Uch xonali son kiriting"); 
-let son2 = Math.floor(son1 / 10);
-let yangiSon = (son1 % 10) * 100 + son2;
-document.write(yangiSon)
+const kiritilganParol = prompt("Parolni kiriting:"); 
+const togriParol = "najottalim"; 
+if (kiritilganParol === togriParol) {
+  
+} else {
+  window.close()
+}
+
+const hisoblash = document.getElementById("btn");
+hisoblash.addEventListener("click", () => {
+  const sonInput = document.getElementById("input1");
+  const son = Number(sonInput.value);
+
+  let onliklar = Math.floor((son / 10) % 10); 
+  let yuzliklar = Math.floor(son / 100); 
+  let yangiSon = son - yuzliklar * 100 + onliklar * 100 + yuzliklar;
+
+  console.log(yangiSon); // Yangi sonni ekranga chiqarish
+  const natijaElement = document.getElementById("natija");
+  natijaElement.textContent = "Natija: " + yangiSon;
+});
+
+
+const son = prompt("Katta sonni kiriting:"); 
+const sonNumber = Number(son); 
+
+const yuzliklar = Math.floor(sonNumber / 100);    
+
+console.log("Yuzliklar xonasidagi son:", yuzliklar);      
